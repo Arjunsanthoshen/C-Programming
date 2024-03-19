1f063a6e8294e5dbf9242e9eeaef9c0454e8df59
ARJUN SANTHOSH E N <116294877+Arjunsanthoshen@users.noreply.github.com>
2024-03-19 23:50:26 +0530

    Add files via upload

---
+++ Adress.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    printf("Name: Arjun");
+    printf("\nAddress: Chaithram, punnol ");
+    printf("\n city: Thalassery"); 
+    printf("\n phone: 8891323747");
+    return 0;
+}
